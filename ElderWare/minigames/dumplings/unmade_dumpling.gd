bce6536e177dd70c2f8931ca33ff47c1b4d696e7
Hisdri <hisdri@proton.me>
2025-04-13 00:19:00 +0200

    dumplings

--- ElderWare/minigames/dumplings/unmade_dumpling.gd
+++ ElderWare/minigames/dumplings/unmade_dumpling.gd
@@ -1,11 +1,14 @@
 extends Area2D
 
+const dumpling = preload("res://minigames/dumplings/dumpling.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_area_entered(area: Area2D):
+	if area.is_in_group("filling"):
+		var spawn_pos = self.global_position
+		queue_free()
+		area.queue_free()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		if dumpling:
+			var new_instance = dumpling.instantiate()
+			get_tree().current_scene.add_child(new_instance)
+			new_instance.global_position = spawn_pos
