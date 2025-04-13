29447d6858c3b804bc1f5d25e280cf6cb127bbc6
TaTeIsMe <tatepon2@gmail.com>
2025-04-13 04:28:52 +0200

    add minigame announcements

--- ElderWare/main.gd
+++ ElderWare/main.gd
@@ -6,15 +6,16 @@ extends Node2D
 
 var score = 0
 const MINIGAMES = [
-	preload("res://minigames/test/test.tscn"),
-	preload("res://minigames/pills/getyourmeds.tscn"),
-	preload("res://minigames/police/police.tscn"),
-	preload("res://minigames/stroke_that_cat/stroke_that_cat.tscn"),
-	preload("res://minigames/dumplings/make_some_dumplings.tscn"),
+	{"scene":preload("res://minigames/test/test.tscn"), "message":"test"},
+	{"scene":preload("res://minigames/pills/getyourmeds.tscn"),"message":"Get your meds!"},
+	{"scene":preload("res://minigames/police/police.tscn"),"message":"Call the police!"},
+	{"scene":preload("res://minigames/stroke_that_cat/stroke_that_cat.tscn"),"message":"Stroke that cat!"},
+	{"scene":preload("res://minigames/dumplings/make_some_dumplings.tscn","message":"Make dumplings!")}
 ]
 
 var clock
 var cameraposin
+var minigame_dict
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -22,6 +23,7 @@ func _ready() -> void:
 	clock = $GrandfatherClock.get_child(1)
 	clock.time_ended.connect(self._on_minigame_failed)
 	cameraposin = $Camera.zoom
+	minigame_dict = MINIGAMES.pick_random()
 	self._next_minigame()
 
 func _clear_minigame():
@@ -30,21 +32,22 @@ func _clear_minigame():
 		child.queue_free()
 
 func _next_minigame():
-	var minigame = MINIGAMES.pick_random().instantiate()
+	var minigame = minigame_dict.scene.instantiate()
 	minigame.mingame_failed.connect(self._on_minigame_failed)
 	minigame.minigame_complete.connect(self._on_minigame_complete)
+
 	self._clear_minigame()
 	$MinigameContainer.add_child(minigame) 
 	clock.reset()
 
 func _on_minigame_failed():
+	minigame_dict = MINIGAMES.pick_random()
 	self.zoom_out()
 	self._clear_minigame()
-	porażka_dot_wav.play()
 	score -= 1
 	$Status.text = "Porażka"
 	$Status.visible = true
-	$Score.text = "Score: %s" % score
+	$Status.text = minigame_dict.message
 	clock.kill_babushka()
 	await get_tree().create_timer(2.0).timeout
 	self.zoom_in()
@@ -52,11 +55,12 @@ func _on_minigame_failed():
 	self._next_minigame()
 
 func _on_minigame_complete():
+	minigame_dict = MINIGAMES.pick_random()
 	self.zoom_out()
 	self._clear_minigame()
 	wygrana_dot_wav.play()
 	score += 1
-	$Status.text = "Tak trzymaj!"
+	$Status.text = minigame_dict.message
 	$Status.visible = true
 	$Score.text = "Score: %s" % score
 	clock.yipie()
